4c8187776ea3c9599fcb833c7d2286080ce01f35
Abhishek Sharma <aatish3670@gmail.com>
2021-09-16 20:12:53 +0530

    first commit

--- max and min/maxAndmin.cpp
+++ max and min/maxAndmin.cpp
@@ -3,15 +3,24 @@
 #include <vector>
 
 using namespace std;
-vector<int> maxandmin(vector<int> v)
+
+vector<pair<int, int>> maxandmin(vector<int> v)
 {
-    sort(v.begin(), v.end());
-    return v;
+    vector<pair<int, int>> ans;
+    int maxi = INT_MIN;
+    int mini = INT_MAX;
+    for (int i = 0; i < v.size(); i++)
+    {
+        maxi = max(maxi, v[i]);
+        mini = min(maxi, v[i]);
+    }
+    ans.push_back(make_pair(maxi, mini));
+    return ans;
 }
 int main()
 {
     vector<int> v{1, 334, 2, 32, 5, 0};
-    vector<int> ans;
+    vector<pair<int, int>> ans;
     ans = maxandmin(v);
-    cout << "Maximum and Minimum in the array : " << ans[v.size() - 1] << " and " << ans[0] << endl;
+    cout << ans[0].first << " " << ans[0].second << endl;
 }
